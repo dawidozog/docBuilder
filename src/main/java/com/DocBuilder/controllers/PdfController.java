2e5fb372dd832bbeb77b1831da6f7b9070840ab9
DawidOzog <dawid.ozog@tronik.pl>
2025-03-23 19:37:33 +0100

    init

--- src/main/java/com/DocBuilder/controllers/PdfController.java
+++ src/main/java/com/DocBuilder/controllers/PdfController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/pdf")
+@RequestMapping("/api/pdf")
 public class PdfController {
     private final PdfService pdfService;
 
